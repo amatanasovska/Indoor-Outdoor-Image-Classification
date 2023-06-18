839ae223b70ca48a4882f1bbd39b7529efa44032
Ana Marija <amatanasovska@yahoo.com>
2023-06-18 14:17:43 +0200

    Model,views,form implementation

--- imageprocessing/classification/views.py
+++ imageprocessing/classification/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from .forms import PredictionForm
+from. models import Prediction
 
-# Create your views here.
+def predict(request: HttpRequest):
+    context = dict()
+    context["form"] = PredictionForm
+
+    # if request.method == "POST":
+    #     form_data = PredictionForm(data=request.POST, files=request.FILES)
+    #     if form_data.is_valid():
+    #         flight: Flight = form_data.save(commit=False)
+    #         flight.user = request.user
+    #         flight.picture = form_data.cleaned_data["picture"]
+    #         flight.save()
+    #         return redirect("flights")
+    # context["flights"] = Flight.objects.all()
+    return render(request, "prediction.html", context=context)
